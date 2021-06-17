89d81f2822d8bdad8381a9e97d5f8fa556dbcc28
Kamil Put <kamil.put@interia.pl>
2021-06-17 09:48:14 +0200

    Create class component, add PropTypes

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
+    image: PropTypes.string.isRequired,
+  }
+
+  static defaultProps = {
+    children: <p>Interesting things I want to check out!</p>,
+  }
+  
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} splashImage={this.props.image} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>  
+          <Column columnTitle={'Animals'} />
+          <Column columnTitle={'Plants'} />
+          <Column columnTitle={'Minerals'} />
+        </div>
       </section>
     );
   }
